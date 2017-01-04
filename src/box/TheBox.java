0db358e8186b5257e722e7ce9eee660b485668de
GamesPower <games.ultrapower@gmail.com>
2017-01-04 22:02:31 +0100

    Adding server management

--- src/box/TheBox.java
+++ src/box/TheBox.java
@@ -1,5 +1,11 @@
 package box;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import box.minecraft.MinecraftServer;
+import box.minecraft.exception.ServerStopException;
 import box.web.WebServer;
 import net.sociuris.configuration.ConfigurationFile;
 
@@ -11,23 +17,40 @@ public class TheBox {
 		return TheBox.instance;
 	}
 
-	//private final Logger logger = Logger.getLogger();
+	// private final Logger logger = Logger.getLogger();
 	private final ConfigurationFile settings;
 	private final WebServer webServer;
+	private final Map<String, MinecraftServer> minecraftServerMap = new HashMap<String, MinecraftServer>();
 
 	public TheBox(ConfigurationFile configurationFile) {
 		this.settings = configurationFile;
-		
+
 		this.webServer = new WebServer(configurationFile.getSection("webServer"));
 		this.webServer.start();
 
 		TheBox.instance = this;
 	}
 
-	public void stopServers() {
-		
+	public void stopServers() throws ServerStopException {
+		for(Entry<String, MinecraftServer> entry : minecraftServerMap.entrySet())
+			entry.getValue().stop();
 	}
 	
+	public MinecraftServer getServer(String name) {
+		return minecraftServerMap.get(name);
+	}
+	
+	public void removeServer(MinecraftServer minecraftServer) {
+		if(!minecraftServer.isStarted())
+			minecraftServerMap.remove(minecraftServer);
+	}
+	
+	public MinecraftServer createServer(String name, String jarFile) {
+		MinecraftServer minecraftServer = new MinecraftServer(this, name, jarFile);
+		minecraftServerMap.put(minecraftServer.getName(), minecraftServer);
+		return minecraftServer;
+	}
+
 	public ConfigurationFile getSettings() {
 		return settings;
 	}
