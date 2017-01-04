8c050f512fe3232e9eb69b7576ac9ef80d1dd259
GamesPower <games.ultrapower@gmail.com>
2017-01-04 22:01:28 +0100

    Create console handler

---
+++ src/box/ConsoleHandler.java
@@ -0,0 +1,27 @@
+package box;
+
+import java.io.PrintStream;
+
+public class ConsoleHandler {
+
+	private ConsoleHandler() {
+	}
+
+	private static final PrintStream OUT = System.out;
+
+	public static void handle(TheBox theBox, String line) {
+		if (line.equals("stop")) {
+			System.exit(0);
+		} else {
+			final String lineSeparator = System.lineSeparator();
+			StringBuilder builder = new StringBuilder();
+			if (!line.equalsIgnoreCase("help"))
+				builder.append("Unknown command. ");
+			builder.append("List of commands:").append(lineSeparator);
+			builder.append("\t/help : show this message").append(lineSeparator);
+			builder.append("\t/stop");
+			OUT.println(builder.toString());
+		}
+	}
+
+}
