bb10c14b9420f12383ed47069b28d90998b5ae8a
GamesPower <games.ultrapower@gmail.com>
2017-01-04 21:17:18 +0100

    Adding the configuration file

--- src/box/TheBox.java
+++ src/box/TheBox.java
@@ -6,23 +6,34 @@ import net.sociuris.configuration.ConfigurationFile;
 public class TheBox {
 
 	private static TheBox instance;
+
 	public static TheBox get() {
 		return TheBox.instance;
 	}
-	
+
+	//private final Logger logger = Logger.getLogger();
+	private final ConfigurationFile settings;
 	private final WebServer webServer;
-	
+
 	public TheBox(ConfigurationFile configurationFile) {
-		TheBox.instance = this;
+		this.settings = configurationFile;
+		
 		this.webServer = new WebServer(configurationFile.getSection("webServer"));
 		this.webServer.start();
+
+		TheBox.instance = this;
+	}
+
+	public void stopServers() {
+		
 	}
 	
+	public ConfigurationFile getSettings() {
+		return settings;
+	}
+
 	public WebServer getWebServer() {
 		return webServer;
 	}
 
-	public void stopServers() {
-	}
-	
 }
