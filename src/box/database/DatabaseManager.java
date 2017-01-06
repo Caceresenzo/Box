1f5585e0d27999b6c8197890e7058b9edc612070
GamesPower <games.ultrapower@gmail.com>
2017-01-06 21:58:05 +0100

    Fix database manager

--- src/box/database/DatabaseManager.java
+++ src/box/database/DatabaseManager.java
@@ -12,7 +12,7 @@ public class DatabaseManager {
 	public static DatabaseManager getManager() {
 		return DATABASE_MANAGER;
 	}
-	
+
 	public static Database WEB;
 	public static Database DATA;
 
@@ -23,6 +23,8 @@ public class DatabaseManager {
 
 	public void loadDatabase() {
 		try {
+			Class.forName("org.sqlite.JDBC");
+
 			if (DatabaseManager.WEB != null && WEB.isConnected())
 				WEB.closeConnection();
 
@@ -34,7 +36,7 @@ public class DatabaseManager {
 
 			WEB.openConnection();
 			DATA.openConnection();
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			CrashReport.makeCrashReport("An error occurred while creating default databases!", e);
 		}
 	}
@@ -43,7 +45,7 @@ public class DatabaseManager {
 		try {
 			if (WEB.isConnected()) {
 				StringBuilder builder = new StringBuilder();
-				//builder.append("DROP TABLE IF EXISTS 'web_sessions';");
+				builder.append("DROP TABLE IF EXISTS 'web_sessions';");
 				builder.append("CREATE TABLE IF NOT EXISTS 'web_sessions' (");
 				builder.append("'session_id' varchar(255) NOT NULL,");
 				builder.append("'session_ip' varchar(16) NOT NULL,");
@@ -51,10 +53,15 @@ public class DatabaseManager {
 				builder.append("'session_token' varchar(100) NOT NULL,");
 				builder.append("PRIMARY KEY ('session_id')");
 				builder.append(");");
-				/*String query = "" + "DROP TABLE IF EXISTS `web_session`;" + "CREATE TABLE IF NOT EXISTS `web_session` ("
-						+ "  `session_id` varchar(100) NOT NULL," + "  `session_ip` varchar(16) NOT NULL,"
-						+ "  `session_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-						+ "  `session_token` varchar(100) NOT NULL," + "  PRIMARY KEY (`session_id`)" + ");";*/
+				/*
+				 * String query = "" + "DROP TABLE IF EXISTS `web_session`;" +
+				 * "CREATE TABLE IF NOT EXISTS `web_session` (" +
+				 * "  `session_id` varchar(100) NOT NULL," +
+				 * "  `session_ip` varchar(16) NOT NULL," +
+				 * "  `session_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+				 * + "  `session_token` varchar(100) NOT NULL," +
+				 * "  PRIMARY KEY (`session_id`)" + ");";
+				 */
 
 				int response = WEB.executeUpdate(builder.toString());
 				logger.debug("Database response: %s", response);
