f2bb8f91629255ad91a1a0ebb1a05d22c5a40090
Enzo Caceres <caceresenzo1502@gmail.com>
2017-01-06 23:16:28 +0100

    Test Main

--- src/box/Test.java
+++ src/box/Test.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.sql.SQLException;
 
 import box.database.DatabaseManager;
+import box.web.users.SimpleUser;
+import box.web.users.UserManager;
 import net.sociuris.crash.CrashReport;
 import net.sociuris.logger.Logger;
 
@@ -23,18 +25,21 @@ public class Test {
 		try {
 			DatabaseManager.WEB.executeUpdate(
 					"INSERT INTO 'web_sessions' ('session_id', 'session_ip', 'session_token') VALUES ('key', '90.19.73.219', 'gfe54546e4')");
+			//DatabaseManager.DATA.executeUpdate(
+			//		"INSERT INTO 'users' ('username', 'password', 'salt', 'token') VALUES ('enzo', 'pass', 'salty', 'token:caca')");
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		}
-		
-		try {
-			DatabaseManager.WEB.closeConnection();
-			DatabaseManager.DATA.closeConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 
 		Logger.getLogger().info("IT WORKS!");
+		
+		
+		/*
+		 * 
+		 */
+		SimpleUser user = new SimpleUser("enzo", "pass");
+		UserManager manager = UserManager.getManager();
+		Logger.getLogger().info(":: " + user.getId() + " ::::: " + manager.checkUser("enezo", "paess"));
 	}
 
 }
