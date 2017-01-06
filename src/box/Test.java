240bcefe8a47b44fd95aa543403231ee64a14ebf
GamesPower <games.ultrapower@gmail.com>
2017-01-06 21:49:48 +0100

    Update test

--- src/box/Test.java
+++ src/box/Test.java
@@ -1,16 +1,38 @@
 package box;
 
+import java.io.File;
+import java.sql.SQLException;
+
 import box.database.DatabaseManager;
-import box.utils.RandomUtils;
+import net.sociuris.crash.CrashReport;
+import net.sociuris.logger.Logger;
 
 public class Test {
-	
+
 	private static DatabaseManager dbmanager = DatabaseManager.getManager();
-	
+	private static File workingDirectory = new File(System.getProperty("user.dir", "."));
+
 	public static void main(String[] args) {
-		System.out.println(RandomUtils.getRandomString(100));
-		dbmanager.setupDatabase();
+Logger.getLogger().setDebug(true);
+		CrashReport.setDirectory(new File(workingDirectory, "crash-reports"));
+		Logger.getLogger().setDirectory(new File(workingDirectory, "logs"));
+		dbmanager.loadDatabase();
 		dbmanager.initDatabase();
+		try {
+			DatabaseManager.WEB.executeUpdate("INSERT INTO 'web_sessions' ('session_id', 'session_ip', 'session_token') VALUES ('key', '90.19.73.219', 'gfe54546e4')");
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			DatabaseManager.WEB.closeConnection();
+			DatabaseManager.DATA.closeConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Logger.getLogger().info("IT WORKS!");
 	}
-	
+
 }
