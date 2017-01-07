11cd0e22d81b0974c73a9f7360d4483a464fdc21
Enzo Caceres <caceresenzo1502@gmail.com>
2017-01-07 20:06:45 +0100

    BIG FIX, Startup Fixed

--- src/box/minecraft/MinecraftServer.java
+++ src/box/minecraft/MinecraftServer.java
@@ -1,57 +1,87 @@
 package box.minecraft;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import box.Bootstrap;
 import box.TheBox;
 import box.minecraft.exception.ServerStopException;
 import box.minecraft.exception.StartServerException;
+import box.utils.FileUtils;
+import box.utils.RandomUtils;
 import net.sociuris.configuration.ConfigurationSection;
 import net.sociuris.logger.Logger;
 
 public class MinecraftServer {
-
+	
 	private static String startCommand = "";
 
 	static {
 		ConfigurationSection mcServerSection = TheBox.PROPERTIES.getSection("minecraftServer");
-		final String lineSeparator = System.lineSeparator();
+		final String lineSeparator = File.separator;
 		StringBuilder builder = new StringBuilder();
-		builder.append(mcServerSection.getProperty("javaPath").getAsString()).append(lineSeparator);
+		builder.append("\"" + mcServerSection.getProperty("javaPath").getAsString()).append(lineSeparator);
 		builder.append("bin").append(lineSeparator);
-		builder.append("java ");
-		builder.append(mcServerSection.getProperty("jvmArguments")).append(' ');
-		builder.append("%{JAR_FILE_NAME}").append(' ');
-		builder.append(mcServerSection.getProperty("additionalArguments")).append(' ');
+		builder.append("java.exe\"");
+		builder.append(" " + mcServerSection.getProperty("jvmArguments").getAsString()).append(' ');
+		builder.append("-jar \"%{JAR_FILE_NAME}\"").append(' ');
+		builder.append("" + mcServerSection.getProperty("additionalArguments").getAsString()).append(' ');
+		startCommand = builder.toString();
 	}
 
 	private final Logger logger = Logger.getLogger();
 	// private final TheBox theBox;
 	private final String name;
 	private final String jarFile;
+	
+	private final Runtime runtime = Runtime.getRuntime();
 
 	private final List<String> logs = new ArrayList<String>();
 
 	private Process process;
 	private PrintWriter writer;
+	
 
-	public MinecraftServer(TheBox theBox, String name, String jarFile) {
+	private String owner;
+	private String[] subowner;
+	
+	public MinecraftServer(TheBox theBox, String name, String jarFile, String owner, String[] subowner) {
 		this.name = name;
 		// this.theBox = theBox;
 		this.jarFile = jarFile;
+		this.owner = owner;
+		this.subowner = subowner;
+	}
+	
+	public MinecraftServer(TheBox theBox, String name, String jarFile, String owner) {
+		this(theBox, owner, owner, owner, null);
 	}
 
 	public void start(String additionalArguments) throws Exception {
 		if (!isStarted()) {
-			ProcessBuilder processBuilder = new ProcessBuilder(
-					MinecraftServer.startCommand.replace("%{JAR_FILE_NAME}", jarFile) + additionalArguments);
-
+			Integer port = RandomUtils.getRandomInt(25500, 25600);
+			String finishedStartCommand = MinecraftServer.startCommand.replace("%{JAR_FILE_NAME}", jarFile) + additionalArguments + " --port " + port;
+			System.out.println(":: " + finishedStartCommand + "\n PORT: " + port);
+			//ProcessBuilder processBuilder = new ProcessBuilder(finishedStartCommand);
+			File serverWorkingDirectory = new File(Bootstrap.getWorkingDirectory().getAbsolutePath() + "\\servers\\" + port);
+			File serverEulaFile = new File(serverWorkingDirectory.getAbsolutePath() + "\\eula.txt");
+			FileUtils.createDirectoryIfNotExists(serverWorkingDirectory);
+			if (!serverEulaFile.exists()) {
+				serverEulaFile.createNewFile();
+				BufferedWriter writer = new BufferedWriter(new FileWriter(serverEulaFile));
+				writer.write("eula=true");
+				writer.close();
+			}
+			Process process = runtime.exec(finishedStartCommand, new String[]{""}, serverWorkingDirectory);
 			logger.debug("Starting server %s...", name);
-			this.process = processBuilder.start();
+			this.process = process;
 			this.writer = new PrintWriter(process.getOutputStream());
 
 			new Thread("Server input handler") {
@@ -90,6 +120,11 @@ public class MinecraftServer {
 		writer.flush();
 	}
 
+	public void sendMessage(String message) {
+		writer.println("tell @a [Box]" + message);
+		writer.flush();
+	}
+
 	public void appendLog(String log) {
 		// update web client
 		logs.add(log);
@@ -106,6 +141,14 @@ public class MinecraftServer {
 
 	public String getName() {
 		return name;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+	
+	public String[] getSubOwner() {
+		return subowner;
 	}
 
 }
