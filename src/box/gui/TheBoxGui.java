afc35f7584488e8173db236edbf43ca480cd2049
GamesPower <games.ultrapower@gmail.com>
2017-01-08 00:25:39 +0100

    Adding a properly way to close and static function to create and get this

--- src/box/gui/TheBoxGui.java
+++ src/box/gui/TheBoxGui.java
@@ -1,5 +1,7 @@
 package box.gui;
 
+import java.awt.Desktop;
+
 import box.TheBox;
 import box.resources.ResourcesManager;
 import javafx.application.Application;
@@ -9,27 +11,48 @@ import javafx.stage.Stage;
 
 public class TheBoxGui extends Application {
 
+	private static TheBoxGui instance = null;
+
+	public static TheBoxGui createGui() {
+		if(Desktop.isDesktopSupported() && instance == null)
+			Application.launch(TheBoxGui.class);
+		return instance;
+	}
+
+	public static boolean hasGui() {
+		return (instance != null);
+	}
+
 	private final TheBoxWebBrowser webBrowser = new TheBoxWebBrowser();
+
+	private TheBoxGui() {
+		TheBoxGui.instance = this;
+	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("TheBox v" + TheBox.VERSION);
 		stage.getIcons().add(new Image(ResourcesManager.getResourceAsStream("icon.png")));
-		
+
 		Scene scene = new Scene(webBrowser, 800, 600);
 		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.centerOnScreen();
 
-		webBrowser.load("http://127.0.0.1:" + TheBox.PROPERTIES.getSection("webServer").getProperty("port").getAsInteger());
+		webBrowser.load(
+				"http://127.0.0.1:" + TheBox.PROPERTIES.getSection("webServer").getProperty("port").getAsInteger());
 
 		stage.show();
 	}
-	
+
 	@Override
 	public void stop() throws Exception {
 		super.stop();
 		System.exit(0);
+	}
+	
+	public void stopProperly() throws Exception {
+		super.stop();
 	}
 
 	public TheBoxWebBrowser getWebBrowser() {
