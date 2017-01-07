80eb590ef10a7f3e10e5ae7a446ac8eca7edd94b
GamesPower <games.ultrapower@gmail.com>
2017-01-07 23:30:08 +0100

    Optimize server manager

--- src/box/minecraft/ServerManager.java
+++ src/box/minecraft/ServerManager.java
@@ -1,95 +1,94 @@
 package box.minecraft;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
 
 import box.TheBox;
 import box.minecraft.exception.ServerStopException;
+import box.web.users.User;
+import net.sociuris.logger.Logger;
 
 public class ServerManager {
-	
-	public static ServerManager manager = new ServerManager();
-	
-	public List<MinecraftServer> servers = new ArrayList<MinecraftServer>();
-	
-	private ServerManager() {
-		if (checkList()) {
-			for (MinecraftServer server : servers) {
-				try {
-					if (server.isStarted()) {
-						server.sendMessage("tellraw @a [\"\",{\"text\":\"[Box] \",\"color\":\"blue\",\"bold\":true,\"clickEvent\":{\"action\":\"open_url\",\"value\":\"\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"The Box™\"}]}}},{\"text\":\"Box's ServerManager instance recreating.... Stopping Server...\",\"color\":\"none\",\"bold\":false}]");
-						server.sendCommand("save all");
-						server.stop();
-					}
-				} catch (ServerStopException exception) {
-					exception.printStackTrace();
+
+	private final Logger logger = Logger.getLogger();
+	private final List<MinecraftServer> serverList = new ArrayList<MinecraftServer>();
+
+	public ServerManager() {
+		for (MinecraftServer server : serverList) {
+			try {
+				if (server.isStarted()) {
+					server.sendMessage(
+							"tellraw @a [\"\",{\"text\":\"[Box] \",\"color\":\"blue\",\"bold\":true,\"clickEvent\":{\"action\":\"open_url\",\"value\":\"\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"The Box™\"}]}}},{\"text\":\"Box's ServerManager instance recreating.... Stopping Server...\",\"color\":\"none\",\"bold\":false}]");
+					server.sendCommand("save all");
+					server.stop();
 				}
+			} catch (ServerStopException exception) {
+				exception.printStackTrace();
 			}
 		}
-		servers = new ArrayList<MinecraftServer>();
 	}
-	
-	public static ServerManager getManager() {
-		return manager;
+
+	public MinecraftServer createServer(String name, int port, String jarFilePath, User owner, User[] operators) {
+		if (port == -1 || port < 1024 || port > 65535)
+			port = this.getAvailablePort();
+		MinecraftServer minecraftServer = new MinecraftServer(TheBox.get(), name, jarFilePath, owner, operators);
+		if (!serverList.contains(minecraftServer)) {
+			logger.info("Creating Minecraft server: %s", minecraftServer.toString());
+			serverList.add(minecraftServer);
+			return minecraftServer;
+		} else
+			return getServer(name);
 	}
-	
-	public MinecraftServer createServer(String name, String jarFile, String owner, String[] subowner) {
-		MinecraftServer minecraftServer = new MinecraftServer(TheBox.get(), name, jarFile, owner, subowner);
-		if (checkList()) {
-			boolean alreadyNamed = false;
-			for (MinecraftServer server : servers) {
-				if (server.getName() == name && alreadyNamed == false) {
-					alreadyNamed = true;
-					return server;
-				}
-			}
-		}
-		servers.add(minecraftServer);
-		return minecraftServer;
+
+	public boolean removeServer(MinecraftServer server) {
+		if (!server.isStarted())
+			return serverList.remove(server);
+		else
+			return false;
 	}
-	
+
 	public MinecraftServer getServer(String name) {
-		if (checkList()) {
-			for (MinecraftServer server : servers) {
-				if (server.getName().equals(name)) {
-					return server;
-				}
-			}
-		}
+		for (MinecraftServer server : serverList)
+			if (server.getName().equals(name))
+				return server;
 		return null;
 	}
-	
-	public boolean addServer(MinecraftServer server) {
-		if (checkList() && !servers.contains(server)) {
-			return servers.add(server);
-		}
-		return false;
+
+	public boolean hasServers() {
+		return (!serverList.isEmpty());
 	}
-	
-	public boolean removeServer(MinecraftServer server) {
-		if (checkList() && servers.contains(server) && server.isStarted()) {
-			return servers.remove(server);
-		}
-		return false;
-	}
-	
+
 	public List<MinecraftServer> getServers() {
-		return this.servers;
+		return serverList;
 	}
-	
+
 	public void stopServers() throws ServerStopException {
-		if (checkList()) {
-			for (MinecraftServer server : servers) {
+		ServerStopException exception = null;
+		for (MinecraftServer server : serverList) {
+			try {
 				server.stop();
+			} catch (ServerStopException e) {
+				exception = e;
 			}
 		}
+		if (exception != null)
+			throw exception;
 	}
-	
-	private boolean checkList() {
-		if (servers == null) {
-			servers = new ArrayList<MinecraftServer>();
-		}
-		return !servers.isEmpty();
+
+	public int getAvailablePort() {
+		int port = -1;
+		do {
+			try {
+				ServerSocket serverSocket = new ServerSocket(0);
+				port = serverSocket.getLocalPort();
+				serverSocket.close();
+			} catch (IOException e) {
+				continue;
+			}
+		} while (port == -1);
+		return port;
 	}
-	
+
 }
