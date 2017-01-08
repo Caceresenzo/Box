b0cc3735960a21be4a08ccfe3f263b6c4c9296fa
GamesPower <games.ultrapower@gmail.com>
2017-01-08 18:48:51 +0100

    Change character array to non-primitive (char -> Character)

--- src/box/utils/StringUtils.java
+++ src/box/utils/StringUtils.java
@@ -10,13 +10,11 @@ import java.util.Map.Entry;
 
 public class StringUtils {
 
-	public static final char[] LOWERCASE_ALPHABET = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	public static final Character[] LOWERCASE_ALPHABET = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-	public static final char[] UPPERCASE_ALPHABET = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	public static final Character[] UPPERCASE_ALPHABET = new Character[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 			'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
-	public static final char[] NUMBER = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 	private StringUtils() {
 	}
