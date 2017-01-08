b59fd0b28908e9ca33ae3c287bb38e967a8cc120
Enzo Caceres <caceresenzo1502@gmail.com>
2017-01-08 02:40:03 +0100

    Added, /server stop <port>

--- src/box/ConsoleHandler.java
+++ src/box/ConsoleHandler.java
@@ -4,6 +4,7 @@ import java.io.PrintStream;
 
 import box.minecraft.MinecraftServer;
 import box.minecraft.ServerManager;
+import box.utils.ObjectUtils;
 
 public class ConsoleHandler {
 	
@@ -27,27 +28,54 @@ public class ConsoleHandler {
 				help(false);
 				break;
 				
-			case "list":
-				avaliable = "servers";
+			case "server":
+				avaliable = "list, stop";
 				if (args.length <= 1 || args[1] == null) {
-					error("missing_argument", avaliable);
-					break;
+					error("missing_argument", avaliable); break;
 				}
 				
 				switch (args[1].toLowerCase()) {
-					case "servers":
+					case "list":
 						StringBuilder builder = new StringBuilder();
 						builder.append("\tSTATUS\t\tPORT\t\tNAME").append("\n");
-						for (MinecraftServer server : servermanager.getServers()) {
+						if (!servermanager.getServers().isEmpty()) {
+							for (MinecraftServer server : servermanager.getServers()) {
+								builder
+									.append("\t| " + (server.isStarted() ? "ON" : "OFF"))
+									.append("\t\t| " + server.getPort())
+									.append("\t\t| " + server.getName())
+									.append("\n");
+							}
+						} else {
 							builder
-								.append("\t| " + (server.isStarted() ? "ON" : "OFF"))
-								.append("\t\t| " + server.getPort())
-								.append("\t\t| " + server.getName())
-								.append("\n");
+							.append("\t| -/-").append("\t\t| 00000").append("\t\t| No Server Avaliable").append("\n");
 						}
-						builder.append("\tSTATUS\t\tPORT\t\tNAME");
+						//builder.append("\tSTATUS\t\tPORT\t\tNAME");
 						response(builder.toString());
 						break;
+						
+					case "stop":
+						if (args.length <= 2 || args[2] == null) {
+							error("need_more_argument", "<Server Port>"); break;
+						}
+						if(!ObjectUtils.isStringInteger(args[2])) {
+							error("invalid_integer", "Do \"/server list\" to see server's port"); break;
+						}
+						Integer port = Integer.parseInt(args[2]);
+						MinecraftServer server = servermanager.getServerByPort(port);
+						if (server == null) {
+							error("server_not_exists", ""); break;
+						}
+						if (server != null && !server.isStarted()) {
+							error("server_not_started", ""); break;
+						}
+						if (!servermanager.stopServerByPort(port)) {
+							error("server_fail_on_stop", ""); break;
+						} else {
+							response("---------- Server Stopped!"); break;
+						}
+						//break; //Unreachable code
+						
 					default:
 						error("invalid_argument", avaliable);
 						break;
@@ -72,7 +100,9 @@ public class ConsoleHandler {
 			builder
 				.append("List of commands:").append(lineSeparator)
 				.append("\t/help : show this message").append(lineSeparator)
-				.append("\t/list : get list of data").append(lineSeparator)
+				.append("\t/server :").append(lineSeparator)
+				.append("\t\tlist : get list of data").append(lineSeparator)
+				.append("\t\tstop <Server Port> : stop a server by the port").append(lineSeparator)
 				.append("\t/stop");
 		OUT.println(builder.toString());
 	}
@@ -85,6 +115,21 @@ public class ConsoleHandler {
 				break;
 			case "missing_argument":
 				response = "Missing Argument. Available: " + info;
+				break;
+			case "need_more_argument":
+				response = "Need More Argument. Add: " + info;
+				break;
+			case "invalid_integer":
+				response = "This value is not an Integer (" + info + ")";
+				break;
+			case "server_not_started":
+				response = "This server is not started.";
+				break;
+			case "server_not_exists":
+				response = "This server does not exists.";
+				break;
+			case "server_fail_on_stop":
+				response = "This server has not been stopped. An error occured.";
 				break;
 			default:
 				response = "Error Occured.";
