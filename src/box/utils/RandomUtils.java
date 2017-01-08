4933a2cb1683ab780aa2eeeab2f3c951611938f9
GamesPower <games.ultrapower@gmail.com>
2017-01-08 18:44:33 +0100

    Adding random double and optimize random string / integer

--- src/box/utils/RandomUtils.java
+++ src/box/utils/RandomUtils.java
@@ -4,22 +4,35 @@ import java.util.Random;
 
 public class RandomUtils {
 
-	public static String getRandomString(Integer length) {
-		final String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ1234567890_"; //"abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
-		StringBuilder result = new StringBuilder();
-		while (length > 0) {
-			Random random = new Random();
-			result.append(characters.charAt(random.nextInt(characters.length())));
+	public static final Random RANDOM = new Random();
+	
+	public static String getString(int length) {
+		final Character[] chars = CollectionUtils.mergeArray(StringUtils.LOWERCASE_ALPHABET, StringUtils.UPPERCASE_ALPHABET);
+		String result = "";
+		while(length > 0) {
+			result += chars[RandomUtils.RANDOM.nextInt(chars.length)];
 			length--;
 		}
-		return result.toString();
+		return result;
 	}
 	
-	public static Integer getRandomInt(Integer min, Integer max) {
-		return getRandomInt(min, max, new Random());
+	public static int getInteger() {
+		return RandomUtils.getInteger(Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
 	
-	public static Integer getRandomInt(Integer min, Integer max, Random random) {
-		return random.nextInt(max - min) + min;
+	public static int getInteger(int min, int max) {
+		return RandomUtils.RANDOM.nextInt(max - min) + min;
 	}
+	
+	public static double getDouble() {
+		return RandomUtils.getDouble(Double.MIN_VALUE, Double.MAX_VALUE);
+	}
+	
+	public static double getDouble(double min, double max) {
+		double value = 0D;
+		for(int i = 0; i < (max - min); i++)
+			value += RANDOM.nextDouble();
+		return value + min;
+	}
+
 }
