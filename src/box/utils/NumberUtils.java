9448f4d04b5628521dcae1bdd821596b15c1673d
GamesPower <games.ultrapower@gmail.com>
2017-01-08 18:15:03 +0100

    Adding constant NUMBER (0-9)

--- src/box/utils/NumberUtils.java
+++ src/box/utils/NumberUtils.java
@@ -2,6 +2,8 @@ package box.utils;
 
 public final class NumberUtils {
 
+	public static final char[] NUMBER = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	
 	private NumberUtils() {}
 	
 	public static boolean isInteger(String s) {
