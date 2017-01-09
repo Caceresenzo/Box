0ec2b19244502db8023b4b463200af0feca45758
GamesPower <games.ultrapower@gmail.com>
2017-01-09 22:22:27 +0100

    Adding web api authentification page

---
+++ src/box/web/api/WebApiPageAuthentification.java
@@ -0,0 +1,21 @@
+package box.web.api;
+
+import java.util.regex.Matcher;
+
+import com.google.gson.JsonObject;
+
+import net.sociuris.http.HttpRequest;
+import net.sociuris.http.HttpResponse;
+import net.sociuris.web.WebSite;
+
+public class WebApiPageAuthentification implements WebApiPage {
+
+	@Override
+	public void writePageContent(WebSite webSite, Matcher uriMatcher, HttpRequest request, HttpResponse response,
+			JsonObject jsonObj) {
+		
+		
+		
+	}
+
+}
