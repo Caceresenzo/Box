bef56b9e9e7ccadf4a8c060f4b96f6e96cb7917a
GamesPower <games.ultrapower@gmail.com>
2017-01-11 22:07:49 +0100

    Adding translation manager

---
+++ src/box/resources/TranslationManager.java
@@ -0,0 +1,11 @@
+package box.resources;
+
+public class TranslationManager {
+
+	public static final String GUI_CLOSE_MESSAGE = getTranslation("gui.close.message");
+	
+	private static String getTranslation(String key) {
+		return key;
+	}
+	
+}
